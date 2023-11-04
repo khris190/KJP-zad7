6e840b001e75fc97ec6a8c266065315382378691
Krzysztof Para <k.para@contina.pl>
2023-11-04 22:02:11 +0100

    add iteration amount const

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 
 #include "my_utils/Logger.hpp"
 #include "my_utils/Profiler.hpp"
+#include <cstddef>
 #include <cstdlib>
 
-long __attribute__((noinline)) foo(long x)
+long foo(long x)
 {
     return x + 1;
 }
-void __attribute__((noinline)) fooRef(long& x)
+void fooRef(long& x)
 {
     x += 1;
 }
-void __attribute__((noinline)) fuu()
+void fuu()
 {
 }
 
@@ -19,38 +20,39 @@ long x = 0;
 long k = 0;
 long y = 0;
 long z = 0;
+const size_t iter = 1000000000;
 int main(int argc, const char** argv)
 {
     logger.setLevel(Level::DEB);
     logger.setTarget(Target::STDOUT);
     {
         newTimer("emptyFor");
-        for (size_t i = 0; i < 100000000; i++) {
+        for (size_t i = 0; i < iter; i++) {
         }
     }
 
     {
         newTimer("function");
-        for (size_t i = 0; i < 100000000; i++) {
+        for (size_t i = 0; i < iter; i++) {
             x = foo(x);
         }
     }
     {
         newTimer("functionRef");
-        for (size_t i = 0; i < 100000000; i++) {
+        for (size_t i = 0; i < iter; i++) {
             fooRef(k);
         }
     }
     {
         newTimer("emptyFn");
-        for (size_t i = 0; i < 100000000; i++) {
+        for (size_t i = 0; i < iter; i++) {
             fuu();
         }
     }
 
     {
         newTimer("raw");
-        for (size_t i = 0; i < 100000000; i++) {
+        for (size_t i = 0; i < iter; i++) {
             y += 1;
         }
     }
